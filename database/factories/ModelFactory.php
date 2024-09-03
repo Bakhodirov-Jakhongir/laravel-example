5747e0ec7017ae8711ebac61577496d806b4b992
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2024-09-03 22:47:37 +0500

    create model and factory for MyModel and

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,18 +2,18 @@
 
 namespace Bakhodirov\LaravelExample\Database\Factories;
 
+use Bakhodirov\Models\MyModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-/*
 class ModelFactory extends Factory
 {
-    protected $model = YourModel::class;
+    protected $model = MyModel::class;
 
     public function definition()
     {
         return [
-
+            'name' => $this->faker->name,
         ];
     }
 }
-*/
