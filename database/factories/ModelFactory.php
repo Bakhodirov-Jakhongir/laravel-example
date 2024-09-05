85e8eb08e0008d998f62ed0b93eace51a92038d7
Jakhongir Bakhodirov <64568158+Bakhodirov-Jakhongir@users.noreply.github.com>
2024-09-05 08:04:20 +0500

    Feature/adding model and migrations (#1)
    * create my_models table
    
    * create model and factory for MyModel and
    
    * write tests for migration
    
    * adding migration and model feature
    
    * Fix styling
    
    ---------
    
    Co-authored-by: Bakhodirov-Jakhongir <Bakhodirov-Jakhongir@users.noreply.github.com>

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,18 +2,17 @@
 
 namespace Bakhodirov\LaravelExample\Database\Factories;
 
+use Bakhodirov\Models\MyModel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/*
 class ModelFactory extends Factory
 {
-    protected $model = YourModel::class;
+    protected $model = MyModel::class;
 
     public function definition()
     {
         return [
-
+            'name' => $this->faker->name,
         ];
     }
 }
-*/
