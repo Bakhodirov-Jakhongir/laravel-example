b634392a5799d30901d9f4b7d0c97866f1f29378
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2024-09-15 11:11:39 +0500

    declare protected string name in MyModel

--- src/Models/MyModel.php
+++ src/Models/MyModel.php
@@ -11,6 +11,8 @@ class MyModel extends Model
 
     public $guarded = [];
 
+    protected string $name;
+
     public function getUppercasedName(): string
     {
         return strtoupper($this->name);
